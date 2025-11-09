d661b76a33bdbfbcf328b99505ba944b2517630f
Marcos Henrique <mhsilva555@gmail.com>
2025-11-08 22:53:01 -0300

    Update tag title nos templates de layout

--- resources/views/components/layouts/dash.blade.php
+++ resources/views/components/layouts/dash.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Dashboard' }}</title>
+    <title>{{ $title." - ".config('app.name') ?? 'Dashboard' }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
