206a6954da46831650f9622b118f7b20b91e5c2c
Marcos Henrique <mhsilva555@gmail.com>
2025-11-08 22:33:27 -0300

    Update diretório de imagens e atualização das diretivas de imagens no templates blade

--- resources/views/livewire/form-login.blade.php
+++ resources/views/livewire/form-login.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-white shadow-md rounded w-full min-h-screen">
     <div class="grid lg:grid-cols-2 min-h-screen">
         <div class="bg-[var(--pollux-red)] min-h-screen hidden lg:block">
-            <img class="h-full object-cover object-center opacity-50" src="{{ Vite::asset('resources/images/enterprise.webp') }}" alt="">
+            <img class="h-full object-cover object-center opacity-50" src="{{ asset('images/enterprise.webp') }}" alt="">
         </div>
         <div class="px-10 lg:p-20 bg-white min-h-screen flex items-center justify-center">
             <livewire:auth.login />
--- resources/views/livewire/logo.blade.php
+++ resources/views/livewire/logo.blade.php
@@ -1,1 +1,1 @@
-<img class="max-w-[150px] mx-auto mb-5" src="{{ Vite::asset('resources/images/logo-pollux.webp') }}" alt="">
+<img class="max-w-[150px] mx-auto mb-5" src="{{ asset('images/logo-pollux.webp') }}" alt="">
